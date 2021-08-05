70cf0a03dc1c06a64b3913c62a4450487ac4b7b8
manpreet <manighotra032@gmail.com>
2021-08-05 18:58:05 +0530

    modified cart item and get max quantity

--- src/main/java/com/wms1/out/OutController.java
+++ src/main/java/com/wms1/out/OutController.java
@@ -15,11 +15,11 @@ public class OutController {
     OutRepo outRepo;
 
     @PostMapping("/generateOrder")
-    public String generateOrder(@RequestBody Out out)
-    {   String message="Unsuccessful";
-        if(outRepo.checkOutData(out.getSales_no(),out.getUser_name()).size()>0){
-            message="Already";
-        }else {
+    public String generateOrder(@RequestBody Out out) {
+        String message = "Unsuccessful";
+        if (outRepo.checkOutData(out.getSales_no(), out.getUser_name()).size() > 0) {
+            message = "Already";
+        } else {
             Out out1 = outRepo.save(out);
             if (out1.getId() != 0) {
                 message = "Successful";
@@ -29,25 +29,26 @@ public class OutController {
     }
 
     @GetMapping("/getSalesNo")
-    public Map<String, List<Out>> getSalesNo(){
-        Map<String, List<Out>> hMap=new HashMap<>();
-        hMap.put("salesNo",outRepo.getOutData());
-        return  hMap;
+    public Map<String, List<Out>> getSalesNo(@RequestParam("user_name") String user_name) {
+        Map<String, List<Out>> hMap = new HashMap<>();
+        hMap.put("salesNo", outRepo.getOutDataStatus0(user_name));
+        return hMap;
     }
- @GetMapping("/getSalesNoWithUser")
-    public Map<String, List<Out>> getSalesNoWithUser( @RequestParam("user_name")String user_name){
-        Map<String, List<Out>> hMap=new HashMap<>();
-        hMap.put("salesNo",outRepo.getOutData());
-        return  hMap;
+
+    @GetMapping("/getSalesNoWithUser")
+    public Map<String, List<Out>> getSalesNoWithUser(@RequestParam("user_name") String user_name) {
+        Map<String, List<Out>> hMap = new HashMap<>();
+        hMap.put("salesNo", outRepo.getOutData(user_name));
+        return hMap;
     }
 
     @PostMapping("/updateSalesNo")
-    public String updateSalesNo(@RequestParam("sales_no") String sales_no,@RequestParam("user_name")String user_name)
-    {   String message = "{\"message\":\"Unsuccessful\"}";
-        int update=outRepo.updateStatus(sales_no,user_name);
-        if(update>0){
-                message = "{\"message\":\"Successful\"}";
+    public String updateSalesNo(@RequestParam("sales_no") String sales_no, @RequestParam("user_name") String user_name) {
+        String message = "{\"message\":\"Unsuccessful\"}";
+        int update = outRepo.updateStatus(sales_no, user_name);
+        if (update > 0) {
+            message = "{\"message\":\"Successful\"}";
         }
-        return  message;
+        return message;
     }
 }
--- src/main/java/com/wms1/out/OutRepo.java
+++ src/main/java/com/wms1/out/OutRepo.java
@@ -11,8 +11,8 @@ import java.util.List;
 @Repository
 public interface OutRepo extends CrudRepository<Out, Integer> {
 
-    @Query("select out from Out out where status=0")
-    List<Out> getOutData();
+    @Query("select out from Out out where status=0 and user_name=?1")
+    List<Out> getOutDataStatus0(String user_name);
 
     @Query("select out from Out out where status=2 and user_name=?1")
     List<Out> getOutData(String user_name);
